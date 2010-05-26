9fddebefcb31ddab4fd1e2457a986e986b266161
pferraro <paul.ferraro@redhat.com>
2010-05-26 17:20:01 +0000

    Test "" and null parsing conditions, including parseSocketAddresses().

--- src/test/java/org/jboss/modcluster/UtilsTestCase.java
+++ src/test/java/org/jboss/modcluster/UtilsTestCase.java
@@ -1,7 +1,9 @@
 package org.jboss.modcluster;
 
+import java.net.InetAddress;
 import java.net.InetSocketAddress;
 import java.net.UnknownHostException;
+import java.util.List;
 
 import org.junit.Assert;
 import org.junit.Test;
@@ -50,5 +52,43 @@ public class UtilsTestCase
       
       Assert.assertEquals("0:0:0:0:0:0:0:1", address.getAddress().getHostAddress());
       Assert.assertEquals(80, address.getPort());
+      
+      address = Utils.parseSocketAddress("", 0);
+      
+      Assert.assertSame(InetAddress.getLocalHost(), address.getAddress());
+      Assert.assertEquals(0, address.getPort());
+      
+      address = Utils.parseSocketAddress(null, 0);
+      
+      Assert.assertSame(InetAddress.getLocalHost(), address.getAddress());
+      Assert.assertEquals(0, address.getPort());
+   }
+
+   @Test
+   public void parseSocketAddresses()
+   {
+      List<InetSocketAddress> addresses = Utils.parseSocketAddresses("192.168.1.11:8000,192.168.1.13:8000", 8001);
+      
+      Assert.assertEquals(2, addresses.size());
+      Assert.assertEquals("192.168.1.11", addresses.get(0).getAddress().getHostAddress());
+      Assert.assertEquals(8000, addresses.get(0).getPort());
+      Assert.assertEquals("192.168.1.13", addresses.get(1).getAddress().getHostAddress());
+      Assert.assertEquals(8000, addresses.get(1).getPort());
+      
+      addresses = Utils.parseSocketAddresses(" 192.168.1.11:8000 , 192.168.1.13:8000 ", 8000);
+      
+      Assert.assertEquals(2, addresses.size());
+      Assert.assertEquals("192.168.1.11", addresses.get(0).getAddress().getHostAddress());
+      Assert.assertEquals(8000, addresses.get(0).getPort());
+      Assert.assertEquals("192.168.1.13", addresses.get(1).getAddress().getHostAddress());
+      Assert.assertEquals(8000, addresses.get(1).getPort());
+      
+      addresses = Utils.parseSocketAddresses("", 8000);
+      
+      Assert.assertEquals(0, addresses.size());
+      
+      addresses = Utils.parseSocketAddresses(null, 8000);
+      
+      Assert.assertEquals(0, addresses.size());
    }
 }
