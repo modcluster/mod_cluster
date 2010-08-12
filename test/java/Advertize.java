ffcc8d0d93311c095b6c57113522d6882e9844dd
jfclere <jfclere@gmail.com>
2010-08-12 11:17:49 +0000

    Arrange tests to make them available in the bundles.

--- test/java/Advertize.java
+++ test/java/Advertize.java
@@ -1,13 +1,48 @@
+/*
+ *  Copyright(c) 2010 Red Hat Middleware, LLC,
+ *  and individual contributors as indicated by the @authors tag.
+ *  See the copyright.txt in the distribution for a
+ *  full listing of individual contributors.
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2 of the License, or (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library in the file COPYING.LIB;
+ *  if not, write to the Free Software Foundation, Inc.,
+ *  59 Temple Place - Suite 330, Boston, MA 02111-1307, USA
+ *
+ * @author Jean-Frederic Clere
+ * @version $Revision: 420067 $, $Date: 2006-07-08 09:16:58 +0200 (sub, 08 srp 2006) $
+ */
 import java.net.MulticastSocket;
 import java.net.InetAddress;
 import java.net.DatagramPacket;
 
 public class Advertize
 {
+    /*
+     * Client to test Advertize... Run it on node boxes to test for firewall.
+     */
     public static void main(String[] args) throws Exception
     {
-        InetAddress group = InetAddress.getByName("224.0.1.105");
-        MulticastSocket s = new MulticastSocket(23364);
+        if (args.length !=2) {
+            System.out.println("Usage: Advertize multicastaddress port");
+            System.out.println("java Advertize 224.0.1.105 23364");
+            System.out.println("receive from 224.0.1.105:23364");
+            System.exit(1);
+        }
+ 
+        InetAddress group = InetAddress.getByName(args[0]);
+        int port = Integer.parseInt(args[1]);
+        MulticastSocket s = new MulticastSocket(port);
         s.setTimeToLive(16);
         s.joinGroup(group);
         boolean ok = true;
