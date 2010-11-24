765a0ab88a1879b89fb072a7d8e46edf0b63a633
jfclere <jfclere@gmail.com>
2010-11-24 15:41:24 +0000

    Oops broken test...

--- test/java/org/jboss/mod_cluster/TestFailAppover.java
+++ test/java/org/jboss/mod_cluster/TestFailAppover.java
@@ -59,12 +59,12 @@ public class TestFailAppover extends TestCase {
 
             service = new JBossWeb("node3",  "localhost");
             connector = service.addConnector(8013);
-            service.AddContext("/test", "/test");
+            service.AddContext("/test", "/test", true);
             server.addService(service);
 
             service2 = new JBossWeb("node4",  "localhost");
             connector2 = service2.addConnector(8014);
-            service2.AddContext("/test", "/test");
+            service2.AddContext("/test", "/test", true);
             server.addService(service2);
 
             cluster = Maintest.createClusterListener("232.0.0.2", 23364, false, "dom1");
@@ -99,7 +99,7 @@ public class TestFailAppover extends TestCase {
 
         // Wait for it.
         try {
-            if (client.runit("/ROOT/MyCount", 10, false, true) != 0)
+            if (client.runit("/test/MyCount", 10, false, true) != 0)
                 clienterror = true;
         } catch (Exception ex) {
             ex.printStackTrace();
@@ -111,10 +111,10 @@ public class TestFailAppover extends TestCase {
         // Stop the connector that has received the request...
         String node = client.getnode();
         if ("node4".equals(node)) {
-            service2.removeContext("/");
+            service2.removeContext("/test");
             node = "node3";
         } else {
-            service.removeContext("/");
+            service.removeContext("/test");
             node = "node4";
         }
 
