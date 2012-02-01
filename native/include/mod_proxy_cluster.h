1bcd33e0eb79aaf03f757b7a45e3d5a0940c206b
jfclere <jfclere@gmail.com>
2012-02-01 14:22:21 +0000

    Fix MODCLUSTER-256.

--- native/include/mod_proxy_cluster.h
+++ native/include/mod_proxy_cluster.h
@@ -28,7 +28,7 @@
 #ifndef MOD_PROXY_CLUSTER_H
 #define MOD_PROXY_CLUSTER_H
 
-#define MOD_CLUSTER_EXPOSED_VERSION "mod_cluster/1.2.x"
+#define MOD_CLUSTER_EXPOSED_VERSION "mod_cluster/1.2.0.Beta5.dev"
 
 /* define the values for sticky_force */
 #define STSESSION 0x01 /* Use sticky session logic (first sessionid and then domain) */
