70b82b783b6c6e6f1463edbd0de0d0fac103f95a
jfclere <jfclere@gmail.com>
2012-04-06 07:44:24 +0000

    Arrange pom. Prepare JGroups tests.

---
+++ test/java/src/test/java/org/jboss/mod_cluster/TestBaseJgroups.java
@@ -0,0 +1,106 @@
+/*
+ *  mod_cluster
+ *
+ *  Copyright(c) 2012 Red Hat Middleware, LLC,
+ *  and individual contributors as indicated by the @authors tag.
+ *  See the copyright.txt in the distribution for a
+ *  full listing of individual contributors.
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2 of the License, or (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library in the file COPYING.LIB;
+ *  if not, write to the Free Software Foundation, Inc.,
+ *  59 Temple Place - Suite 330, Boston, MA 02111-1307, USA
+ *
+ * @author Jean-Frederic Clere
+ * @version $Revision$
+ */
+
+package org.jboss.mod_cluster;
+
+import java.io.IOException;
+
+import junit.framework.TestCase;
+
+import org.apache.catalina.Engine;
+import org.apache.catalina.Service;
+import org.jboss.modcluster.ModClusterService;
+import org.apache.catalina.LifecycleException;
+import org.apache.catalina.connector.Connector;
+import org.apache.catalina.core.StandardServer;
+
+public class TestBaseJgroups extends TestCase {
+
+    /* Test that the sessions are really sticky */
+    public void testBase() {
+
+        boolean clienterror = false;
+        StandardServer server = Maintest.getServer();
+        ModClusterService cluster = null;
+        JBossWeb service = null;
+
+        System.out.println("TestBaseJgroups Started");
+        try {
+            service = new JBossWeb("node1",  "localhost");
+            service.addConnector(8011);
+            server.addService(service);
+            cluster = Maintest.createClusterListener("224.0.1.105", 23364, false, null, true, false, true, "secret");
+        } catch(Exception ex) {
+            ex.printStackTrace();
+            fail("can't start service");
+        }
+
+        // start the server thread.
+        ServerThread wait = new ServerThread(3000, server);
+        wait.start();
+         
+        // Wait until httpd we know about httpd.
+        String [] nodes = new String[1];
+        nodes[0] = "node1";
+        int countinfo = 0;
+        while ((!Maintest.checkProxyInfo(cluster, nodes)) && countinfo < 20) {
+            try {
+                Thread.sleep(3000);
+            } catch (InterruptedException ex) {
+                ex.printStackTrace();
+            }
+            countinfo++;
+        }
+
+        // Stop the jboss and remove the services.
+        try {
+            wait.stopit();
+            wait.join();
+            server.removeService(service);
+        } catch (InterruptedException ex) {
+            ex.printStackTrace();
+            fail("can't stop service");
+        }
+        if (clienterror)
+            fail("Client error");
+
+        // Wait until httpd as received the stop messages.
+        countinfo = 0;
+        while ((!Maintest.checkProxyInfo(cluster, null)) && countinfo < 20) {
+            try {
+                Thread.sleep(3000);
+            } catch (InterruptedException ex) {
+                ex.printStackTrace();
+            }
+            countinfo++;
+        }
+        Maintest.StopClusterListener();
+        
+        System.gc();
+        System.out.println("TestBaseJgroups Done");
+    }
+}
