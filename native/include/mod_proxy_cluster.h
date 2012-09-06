d60e713291e04d4888a4e3412a76a6e667d9a2d8
jfclere <jfclere@gmail.com>
2012-09-06 14:26:44 +0000

    ready for release.

--- native/include/mod_proxy_cluster.h
+++ native/include/mod_proxy_cluster.h
@@ -28,7 +28,7 @@
 #ifndef MOD_PROXY_CLUSTER_H
 #define MOD_PROXY_CLUSTER_H
 
-#define MOD_CLUSTER_EXPOSED_VERSION "mod_cluster/1.2.2.Beta1.dev"
+#define MOD_CLUSTER_EXPOSED_VERSION "mod_cluster/1.2.2.Final"
 
 /* define the values for sticky_force */
 #define STSESSION 0x01 /* Use sticky session logic (first sessionid and then domain) */
