600cae40d38c7decaeb42027c7cd1edd389c64df
Jean-Frederic Clere <jfclere@gmail.com>
2013-04-29 15:17:15 +0200

    Fix ap_proxy_make_fake_req in 2.4.x

--- native/mod_proxy_cluster/mod_proxy_cluster.c
+++ native/mod_proxy_cluster/mod_proxy_cluster.c
@@ -1112,6 +1112,45 @@ apr_status_t ap_proxygetline(apr_bucket_brigade *bb, char *s, int n, request_rec
 
     return rv;
 }
+
+/* In 2.4.x the routine is public any more */
+#if AP_MODULE_MAGIC_AT_LEAST(20111203,0)
+static request_rec *ap_proxy_make_fake_req(conn_rec *c, request_rec *r)
+{
+    apr_pool_t *pool;
+    request_rec *rp;
+
+    apr_pool_create(&pool, c->pool);
+
+    rp = apr_pcalloc(pool, sizeof(*r));
+
+    rp->pool            = pool;
+    rp->status          = HTTP_OK;
+
+    rp->headers_in      = apr_table_make(pool, 50);
+    rp->subprocess_env  = apr_table_make(pool, 50);
+    rp->headers_out     = apr_table_make(pool, 12);
+    rp->err_headers_out = apr_table_make(pool, 5);
+    rp->notes           = apr_table_make(pool, 5);
+
+    rp->server = r->server;
+    rp->log = r->log;
+    rp->proxyreq = r->proxyreq;
+    rp->request_time = r->request_time;
+    rp->connection      = c;
+    rp->output_filters  = c->output_filters;
+    rp->input_filters   = c->input_filters;
+    rp->proto_output_filters  = c->output_filters;
+    rp->proto_input_filters   = c->input_filters;
+    rp->useragent_ip = c->client_ip;
+    rp->useragent_addr = c->client_addr;
+
+    rp->request_config  = ap_create_request_config(pool);
+    proxy_run_create_req(r, rp);
+
+    return rp;
+}
+#endif
 /*
  * Do a ping/pong to the node
  */
