6749fb5e0ac1db832abd14c8a1b7c61da6e4ecd8
Radoslav Husar <radosoft@gmail.com>
2014-05-13 15:38:55 +0200

    [MODCLUSTER-405] Make the testsuite a little more portable, introduces -Dmulticast.interface/address1/address2 switches

--- core/src/test/java/org/jboss/modcluster/advertise/AdvertiseListenerImplTestCase.java
+++ core/src/test/java/org/jboss/modcluster/advertise/AdvertiseListenerImplTestCase.java
@@ -60,7 +60,9 @@ public class AdvertiseListenerImplTestCase {
     static {
         System.setProperty("java.net.preferIPv4Stack", "true");
     }
-    private static final String ADVERTISE_GROUP = "224.0.1.106";
+
+    private static final String ADVERTISE_GROUP = System.getProperty("multicast.address1", "224.0.1.106");
+    private static final String ADVERTISE_INTERFACE = System.getProperty("multicast.interface");
     private static final int ADVERTISE_PORT = 23364;
     private static final String SERVER1 = "127.0.0.1";
     private static final String SERVER2 = "127.0.1.1";
@@ -112,14 +114,15 @@ public class AdvertiseListenerImplTestCase {
         byte[] buf = TestUtils.generateAdvertisePacketData(new Date(), 0, SERVER1, SERVER1_ADDRESS);
         DatagramPacket packet = new DatagramPacket(buf, buf.length, this.groupAddress, ADVERTISE_PORT);
 
-        if (!System.getProperty("os.name").startsWith("Windows")) {
+        if (ADVERTISE_INTERFACE != null && !System.getProperty("os.name").startsWith("Windows")) {
             try {
-                InetAddress socketInterface = InetAddress.getLocalHost();
+                InetAddress socketInterface = InetAddress.getByName(ADVERTISE_INTERFACE);
                 this.socket.setInterface(socketInterface);
             } catch (Exception ex) {
                 ; // Ignore it
             }
         }
+
         this.socket.send(packet);
 
         try {
