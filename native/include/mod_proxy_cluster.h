be6fc0e34126ffc49478688ddc039f372c95c5de
Jean-Frederic Clere <jfclere@gmail.com>
2016-06-02 10:45:15 +0200

    bump version
    1.2.13.Final is next.

--- native/include/mod_proxy_cluster.h
+++ native/include/mod_proxy_cluster.h
@@ -28,7 +28,7 @@
 #ifndef MOD_PROXY_CLUSTER_H
 #define MOD_PROXY_CLUSTER_H
 
-#define MOD_CLUSTER_EXPOSED_VERSION "mod_cluster/1.2.12.Final"
+#define MOD_CLUSTER_EXPOSED_VERSION "mod_cluster/1.2.13.Final"
 
 /* define the values for sticky_force */
 #define STSESSION 0x01 /* Use sticky session logic (first sessionid and then domain) */
