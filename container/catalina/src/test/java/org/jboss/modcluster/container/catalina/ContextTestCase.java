4cdfa106a5ca62d7b38be52c3b1a4f15fea23204
Radoslav Husar <radosoft@gmail.com>
2017-01-02 13:53:38 +0100

    MODCLUSTER-560 Upgrade mockito to 2.5.2

--- container/catalina/src/test/java/org/jboss/modcluster/container/catalina/ContextTestCase.java
+++ container/catalina/src/test/java/org/jboss/modcluster/container/catalina/ContextTestCase.java
@@ -25,7 +25,7 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertSame;
 import static org.junit.Assert.assertTrue;
-import static org.mockito.Matchers.same;
+import static org.mockito.ArgumentMatchers.same;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
