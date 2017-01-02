6c3dcd121cb46a5beb112ac91e90950e0915bda1
Radoslav Husar <radosoft@gmail.com>
2017-01-02 16:25:03 +0100

    Avoid ambiguous imports
    [ERROR] both method <T>same(T) in org.mockito.Mockito and method <T>same(T) in org.mockito.Matchers match

--- container/jbossweb/src/test/java/org/jboss/modcluster/container/jbossweb/JBossWebEventHandlerAdapterTestCase.java
+++ container/jbossweb/src/test/java/org/jboss/modcluster/container/jbossweb/JBossWebEventHandlerAdapterTestCase.java
@@ -21,9 +21,12 @@
  */
 package org.jboss.modcluster.container.jbossweb;
 
-import static org.junit.Assert.*;
-import static org.mockito.Matchers.*;
-import static org.mockito.Mockito.*;
+import static org.junit.Assert.assertEquals;
+import static org.mockito.ArgumentMatchers.same;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.reset;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 import javax.management.MBeanServer;
 import javax.management.Notification;
