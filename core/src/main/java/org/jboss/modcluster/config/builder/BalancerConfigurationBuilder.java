2e90c153fc32721bab98f8a2f7017b80ed236153
Radoslav Husar <radosoft@gmail.com>
2017-02-14 18:57:03 +0100

    MODCLUSTER-564 JavaDocs for new package org.jboss.modcluster.config.builder

--- core/src/main/java/org/jboss/modcluster/config/builder/BalancerConfigurationBuilder.java
+++ core/src/main/java/org/jboss/modcluster/config/builder/BalancerConfigurationBuilder.java
@@ -26,6 +26,8 @@ import org.jboss.modcluster.config.BalancerConfiguration;
 import org.jboss.modcluster.config.impl.BalancerConfigurationImpl;
 
 /**
+ * Builder for balancer configuration.
+ *
  * @author Radoslav Husar
  * @since 1.3.6.Final
  */
@@ -41,26 +43,41 @@ public class BalancerConfigurationBuilder extends AbstractConfigurationBuilder i
         super(parentBuilder);
     }
 
+    /**
+     * Enables or disables sticky sessions.
+     */
     public BalancerConfigurationBuilder setStickySession(boolean stickySession) {
         this.stickySession = stickySession;
         return this;
     }
 
+    /**
+     * Sets to remove session when the request cannot be routed to the right node.
+     */
     public BalancerConfigurationBuilder setStickySessionRemove(boolean stickySessionRemove) {
         this.stickySessionRemove = stickySessionRemove;
         return this;
     }
 
+    /**
+     * Sets to return an error when the request cannot be routed to the right node.
+     */
     public BalancerConfigurationBuilder setStickySessionForce(boolean stickySessionForce) {
         this.stickySessionForce = stickySessionForce;
         return this;
     }
 
+    /**
+     * Sets timeout to wait for an available worker (default is no wait).
+     */
     public BalancerConfigurationBuilder setWorkerTimeout(int workerTimeout) {
         this.workerTimeout = workerTimeout;
         return this;
     }
 
+    /**
+     * Sets the maximum number of attempts to send the request to the backend server.
+     */
     public BalancerConfigurationBuilder setMaxAttempts(int maxAttempts) {
         this.maxAttempts = maxAttempts;
         return this;
