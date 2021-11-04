8221d50a481322d6c8cee00da7c1932956c4c804
Radoslav Husar <radosoft@gmail.com>
2021-11-04 23:39:04 +0100

    MODCLUSTER-735 Drop support for Tomcat 7.0

---
+++ container/tomcat85/src/test/java/org/jboss/modcluster/container/tomcat/SimpleServerProviderTestCase.java
@@ -0,0 +1,37 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2017, Red Hat Middleware LLC, and individual contributors
+ * as indicated by the @author tags. See the copyright.txt file in the
+ * distribution for a full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package org.jboss.modcluster.container.tomcat;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+
+import org.apache.catalina.Server;
+import org.junit.Test;
+
+public class SimpleServerProviderTestCase {
+    @Test
+    public void test() {
+        Server server = mock(Server.class);
+        ServerProvider provider = new SimpleServerProvider(server);
+        assertSame(server, provider.getServer());
+    }
+}
